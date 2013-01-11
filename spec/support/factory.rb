a93d60bcadd5df23e31d06c6e9d797f893f9a535
Nathan Long <nathan.long@tma1.com>
2013-01-11 08:48:58 -0500

    Better request specs

--- spec/support/factory.rb
+++ spec/support/factory.rb
@@ -1,7 +1,7 @@
 def insert_employees
   Dossier.client.query('TRUNCATE `employees`')
   [
-    {name: 'Moustafa McMann',  hired_on: '2010-10-02', suspeded: false, division: 'Tedious Toiling',       salary: 30_000 },
+    {name: 'Moustafa McMann',  hired_on: '2010-10-02', suspeded: false, division: 'Zany Inventions',       salary: 30_000 },
     {name: 'Jimmy Jackalope',  hired_on: '2013-01-11', suspeded: true,  division: 'Tedious Toiling',       salary: 20_000 },
     {name: 'Elise Elderberry', hired_on: '2013-01-11', suspeded: false, division: 'Corporate Malfeasance', salary: 99_000 }
   ].each do |employee|
