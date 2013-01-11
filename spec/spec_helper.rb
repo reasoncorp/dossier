753727e39dec6e2b41f4e86a1f0fde56bdd2d4fe
Nathan Long <nathan.long@tma1.com>
2013-01-11 10:04:54 -0500

    Use pry

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
 require 'rspec/rails'
+require 'pry'
 
 Rails.backtrace_cleaner.remove_silencers!
 
