08dbee6d5b05577cb4989ff41ac32f76297affcc
Nathan Long <nathan.long@tma1.com>
2013-01-15 11:32:36 -0500

    Hooray for consistent misspellings!

--- spec/support/factory.rb
+++ spec/support/factory.rb
@@ -1,14 +1,14 @@
 def insert_employees
   Dossier.client.execute('TRUNCATE `employees`')
   [
-    {name: "Moustafa McMann",  hired_on: '2010-10-02', suspeded: false, division: 'Zany Inventions',       salary: 30_000 },
-    {name: 'Jimmy Jackalope, Jr.',  hired_on: '2013-01-11', suspeded: true,  division: 'Tedious Toiling',       salary: 20_000 },
-    {name: 'Elise Elderberry', hired_on: '2013-01-11', suspeded: false, division: 'Corporate Malfeasance', salary: 99_000 }
+    {name: "Moustafa McMann",  hired_on: '2010-10-02', suspended: false, division: 'Zany Inventions',       salary: 30_000 },
+    {name: 'Jimmy Jackalope, Jr.',  hired_on: '2013-01-11', suspended: true,  division: 'Tedious Toiling',       salary: 20_000 },
+    {name: 'Elise Elderberry', hired_on: '2013-01-11', suspended: false, division: 'Corporate Malfeasance', salary: 99_000 }
   ].each do |employee|
     query = <<-QUERY
       INSERT INTO 
         `employees` (`name`, `hired_on`, `suspended`, `division`, `salary`) 
-      VALUES ('#{employee[:name]}', '#{employee[:hired_on]}', #{employee[:suspeded]}, '#{employee[:division]}', #{employee[:salary]});
+      VALUES ('#{employee[:name]}', '#{employee[:hired_on]}', #{employee[:suspended]}, '#{employee[:division]}', #{employee[:salary]});
     QUERY
     Dossier.client.execute(query)
   end
