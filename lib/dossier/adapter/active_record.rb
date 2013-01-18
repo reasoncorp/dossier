9775094e33cb23e43bd2bc1bcc0ae081b6f50950
Nathan Long <nathan.long@tma1.com>
2013-01-18 13:36:39 -0500

    fixed major issue with connection pooling for anonymous active record classes

--- lib/dossier/adapter/active_record.rb
+++ lib/dossier/adapter/active_record.rb
@@ -15,23 +15,28 @@ module Dossier
 
       def execute(query)
         Result.new(connection.execute(query))
-        # TODO: Determine how we can rescue something more specific and still be
-        # neutral about what adapter is used. Mix a common module into all known
-        # exceptions like Mysql2::Error and rescue that module?
-        # rescue ::Mysql2::Error => e
       rescue => e
         raise Dossier::ExecuteError.new "#{e.message}\n\n#{query}"
       end
 
+      # # TODO figure if i can store this w/o the abstract clas
+      # def connection
+      #   @abstract_class.connection
+      # end
+
       private
 
       def active_record_connection
-        klass = Class.new(::ActiveRecord::Base)
-        klass.abstract_class = true
-        klass.establish_connection(options)
-        klass.connection
+        @abstract_class = Class.new(::ActiveRecord::Base) do
+          self.abstract_class = true
+          
+          def self.name
+            object_id.to_s
+          end
+        end
+        @abstract_class.establish_connection(options)
+        @abstract_class.connection
       end
-
 
     end
 
