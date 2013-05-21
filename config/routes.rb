efed6227bcde2db0faa171e8fc1643268d891507
Zach Colon <zach.colon@tma1.com>
2013-05-21 13:05:03 -0400

    refactor MultiReport

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  match "reports/:report", to: 'dossier/reports#show', as: :dossier_report, via: :get
+  get   "reports/:report",       to: 'dossier/reports#show',  as: :dossier_report
   get   "multi/reports/:report", to: 'dossier/reports#multi', as: :multi_report
 
 end
