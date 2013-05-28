9a3754d686b2da8152ae0bde413f9fc17c46a30a
Adam Hunter <adamhunter@me.com>
2013-05-28 09:20:48 -0400

    updating helper for multi report

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   get   "reports/:report",       to: 'dossier/reports#show',  as: :dossier_report
-  get   "multi/reports/:report", to: 'dossier/reports#multi', as: :multi_report
+  get   "multi/reports/:report", to: 'dossier/reports#multi', as: :dossier_multi_report
 
 end
