edfd8997a61243c6dd66126057585f37e6912f25
Adam Hunter <adamhunter@me.com>
2013-05-29 17:24:35 -0400

    added cheap number formatters for performance

--- spec/dossier/formatter_spec.rb
+++ spec/dossier/formatter_spec.rb
@@ -36,4 +36,39 @@ describe Dossier::Formatter do
     end
   end
 
+  describe "non_localized" do
+    describe "number_with_delimiter" do
+      formats = {
+        10_000            => '10,000',
+        10_000.01         => '10,000.01',
+        1_000_000_000.001 => '1,000,000,000.001',
+        '12345.6789'      => '12,345.6789'
+      }.each { |base, formatted|
+        it "formats #{base} as #{formatted}" do
+          expect(formatter.non_localized_number_with_delimiter(base)).to eq formatted
+        end
+      }
+      it "will return the expected precision if too large" do
+        expect(formatter.non_localized_number_with_delimiter(1_000.23523563, 2)).to eq '1,000.24'
+      end
+
+      it "will return the expected precision if too small" do
+        expect(formatter.non_localized_number_with_delimiter(1_000, 5)).to eq '1,000.00000'
+      end
+    end
+
+    describe "number_to_currency" do
+      formats = {
+        10_000            => '$10,000.00',
+        10_000.00         => '$10,000.00',
+        1_000_000_000.000 => '$1,000,000,000.00',
+        '12345.6788'      => '$12,345.68'
+      }.each { |base, formatted|
+        it "formats #{base} as #{formatted}" do
+          expect(formatter.non_localized_number_to_currency(base)).to eq formatted
+        end
+      }
+    end
+  end
+
 end
