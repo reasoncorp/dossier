d0df84c4fd9cd7bd68f82306197248cd29ae858c
Adam Hunter <adamhunter@tma1.com>
2013-12-02 08:40:02 -0500

    cleanup whitespace

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ ENV["RAILS_ENV"] = "test"
 require 'simplecov'
 require 'coveralls'
 
-
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
