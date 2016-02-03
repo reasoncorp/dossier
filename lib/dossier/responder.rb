b34a5dab0dc5b9b28bd9c2886c4faba821471745
estensland <estensland88@gmail.com>
2016-02-02 20:25:25 -0600

    csv and xls now return formatted results instead of just the raw rows. An optional flag might be desired eventually

--- lib/dossier/responder.rb
+++ lib/dossier/responder.rb
@@ -15,25 +15,25 @@ module Dossier
 
     def to_csv
       set_content_disposition!
-      controller.response_body = StreamCSV.new(*collection_and_headers(report.raw_results.arrays))
+      controller.response_body = StreamCSV.new(*collection_and_headers(report.results.arrays))
     end
 
     def to_xls
       set_content_disposition!
-      controller.response_body = Xls.new(*collection_and_headers(report.raw_results.arrays))
+      controller.response_body = Xls.new(*collection_and_headers(report.results.arrays))
     end
 
     def respond
       multi_report_html_only!
       super
     end
-    
+
     private
 
     def set_content_disposition!
       controller.headers["Content-Disposition"] = %[attachment;filename=#{filename}]
     end
-    
+
     def collection_and_headers(collection)
       headers = collection.shift.map { |header| report.format_header(header) }
       [collection, headers]
